dad070fc6ed0ab6f7337e119053219c6e9b480be
tunab <tunabaydemir@gmail.com>
2022-05-17 11:24:10 +0300

    version1

--- Main.java
+++ Main.java
@@ -65,7 +65,7 @@ public class Main {
         }
         ArrayList<Train> trains=new ArrayList<>();
         for(int i=0;i<num_of_hiz_treni;i++){
-            trains.add(new Train(0,"HT-"+i,'A',true));
+            trains.add(new Train(0,"HT-"+i+1,'A',true));
         }
 
         Station stationA=new Station('A');
@@ -76,7 +76,7 @@ public class Main {
         Station stationF=new Station('F');
         Station stationO=new Station('O');
 
-        Graph Graph = new Graph();
+        Graph graph = new Graph();
         stationA.addEdge(new Rail(stationB, 100)); //connect v1 v2
         stationB.addEdge(new Rail(stationA, 100));
 
@@ -95,15 +95,15 @@ public class Main {
         stationF.addEdge(new Rail(stationO, 25)); //connect v4 v5
         stationO.addEdge(new Rail(stationF, 25));
 
-        Graph.addVertex(stationA);
-        Graph.addVertex(stationB);
-        Graph.addVertex(stationC);
-        Graph.addVertex(stationD);
-        Graph.addVertex(stationE);
-        Graph.addVertex(stationF);
-        Graph.addVertex(stationO);
+        graph.addVertex(stationA);
+        graph.addVertex(stationB);
+        graph.addVertex(stationC);
+        graph.addVertex(stationD);
+        graph.addVertex(stationE);
+        graph.addVertex(stationF);
+        graph.addVertex(stationO);
 
-        for (Station st:Graph.getVertices()
+        for (Station st:graph.getVertices()
              ) {
             System.out.println(st.name);
 
@@ -114,23 +114,194 @@ public class Main {
             System.out.println();
 
         }
+        for (int i=0;i<gun*24*60;i++){
+            for (Train train:trains) {
+
+                    if(train.mode==2 ){
+
+                        if (train.maintenance_remaining_time<=0 && graph.getStation(train.waiting_station_name).current_train==null) {
+                            train.working = true;
+                            //graph.getStation(train.waiting_station_name).current_train = train;
+                            int index;
+                            train.mode=0;
+
+
+                        }
+                        else {
+                            train.maintenance_remaining_time=train.maintenance_remaining_time-1;
+                            System.out.println("mainbekleme"+train.maintenance_remaining_time);
+
+                        }
+                    } else if (train.mode==1) {
+                        if (train.service_remaining_time<=0){//yola çıkıcak
+                            train.working = true;
+                            graph.getStation(train.waiting_station_name).current_train = null;
+                            int index;
+                            char next_station_name = 0;
+                            switch (train.type){
+                                case 0:
+                                    index=ROUTE_HIZ_TRENI.indexOf(train.waiting_station_name);
+                                    if ((index==0 && !train.direction) || (index==ROUTE_HIZ_TRENI.length()-1 && train.direction)){
+                                        train.maintenance_remaining_time=120;
+                                        train.mode=2;
+                                        train.direction=!train.direction;
+                                        System.out.println("mode2");
+
+                                        next_station_name=ROUTE_HIZ_TRENI.charAt(train.direction?index+1:index-1);
+                                        System.out.println(next_station_name);
+                                    }
+
+                                    else{
+                                        System.out.println("char"+ROUTE_HIZ_TRENI.charAt(train.direction?index+1:index-1));
+                                        next_station_name=ROUTE_HIZ_TRENI.charAt(train.direction?index+1:index-1);
+                                        train.mode=0;
+                                    }
+                                    break;
+                                case 1:
+                                    index=ROUTE_ANAHAT_TRENI.indexOf(train.waiting_station_name);
+                                    if ((index==0 && !train.direction) || (index==ROUTE_ANAHAT_TRENI.length()-1 && train.direction)) {
+                                        train.maintenance_remaining_time = 240;
+                                        train.mode = 2;
+                                        train.direction=!train.direction;
+
+                                        next_station_name=ROUTE_ANAHAT_TRENI.charAt(train.direction?index+1:index-1);
+                                        System.out.println(next_station_name);                                    }
+                                    else{
+                                            System.out.println("char"+ROUTE_ANAHAT_TRENI.charAt(train.direction?index+1:index-1));
+                                            next_station_name=ROUTE_ANAHAT_TRENI.charAt(train.direction?index+1:index-1);
+                                        train.mode=0;
+                                        }
+                                    break;
+
+                                case 2:
+                                    index=ROUTE_YUK_TRENI.indexOf(train.waiting_station_name);
+                                    if ((index==0 && !train.direction) || (index==ROUTE_YUK_TRENI.length()-1 && train.direction)) {
+                                        train.maintenance_remaining_time=180;
+                                        train.mode=2;
+                                        train.direction=!train.direction;
+
+                                        next_station_name=ROUTE_YUK_TRENI.charAt(train.direction?index+1:index-1);
+                                        System.out.println(next_station_name);
+                                    }
+                                    else{
+                                        System.out.println("char"+ROUTE_YUK_TRENI.charAt(train.direction?index+1:index-1));
+                                        next_station_name=ROUTE_YUK_TRENI.charAt(train.direction?index+1:index-1);
+                                        train.mode=0;
+                                    }
+                                    break;
+
+                            }
+                            for (Iterator<Rail> it = graph.getStation(train.waiting_station_name).edges.iterator(); it.hasNext(); ) {
+                                Rail rail = it.next();
+                                //System.out.println(train.name+graph.getStation(train.waiting_station_name).name+""+rail.to.name+""+next_station_name);
+                                if (rail.to.name==next_station_name) {
+                                    System.out.println("foo found");
+                                    train.remaining_rail = rail.weight;
+                                    train.rail=rail;
+                                    System.out.println(train.rail.to.name+" to");
+
+
+                                    //train.waiting_station_name = 'X';
+                                }
+                            }
+                        }
+                        else{
+
+                            train.service_remaining_time=train.service_remaining_time-1;
+                            System.out.println("bekleme"+train.service_remaining_time);
+                        }
+                    }
+
+                    else {//yolda
+
+
+                            switch (train.type){
+                                case 0:
+                                    train.remaining_rail=train.remaining_rail-(MAX_SPEED_HIZ_TRENI/60.0);
+                                    break;
+
+                                case 1:
+                                    train.remaining_rail=train.remaining_rail-(MAX_SPEED_ANAHAT_TRENI/60.0);
+                                    break;
+
+                                case 2:
+                                    train.remaining_rail=train.remaining_rail-(MAX_SPEED_YUK_TRENI/60.0);
+                                    break;
+
+
+                            }
+                        System.out.println(train.rail.to.name+" "+train.remaining_rail);
+                        if(train.remaining_rail<=0){
+                            if(train.rail.to.current_train==null){
+                                train.rail.to.current_train=train;
+                                train.waiting_station_name=train.rail.to.name;
+                                train.rail=null;
+                                train.mode=1;
+                                train.working = false;
+                                switch (train.type){
+                                    case 0:
+                                        train.service_remaining_time=15;
+                                        break;
+                                    case 1:
+                                        train.service_remaining_time=20;
+                                        break;
+
+                                    case 2:
+                                        train.service_remaining_time=0;
+                                        break;
+
+                                }
+
+                            }
+                            else {
+                                ///////////
+                            }
+                        }
+                        }
+
+                    }
+
+
+            if (i%60==0){
+                System.out.println((i/60)+"-------------------------");
+                for (Train train:trains) {
+                    System.out.println(train.name);
+                    System.out.println(train.waiting_station_name);
+                    System.out.println(train.rail+"\n");
+                }
+
+            }
+
+        }
 
     }
 
     static class Train{
-        int type;
-
-        boolean direction;
+        int type;//0hiz,1anahat,2yuk
+        int mode;//0 yolda, 1 servis bekleme,2 bakım bekleme
+        int service_remaining_time;//istasyondayken kalan zaman
+        int maintenance_remaining_time;//bakımdayken kalan zaman
+        boolean working;//bakımda mı değil mi
+        boolean direction;//bastan sona true, sondan basa false
         String name;
-        int distance;
-        char start_station_name;
-
+        int distance;//bakıma kadar total mesafe
+        char start_station_name;//başlangıc istasyonu
+        char waiting_station_name;//suanda bulunugu istasyon
+        double remaining_rail;//sonraki istasyona kalan yol
+        Rail rail;//suan bulundugu yol
         Train(int type,String name,char start_station_name,boolean direction){
             this.type=type;
             this.name=name;
+            rail=null;
+            service_remaining_time=0;
+            maintenance_remaining_time=0;
+            remaining_rail=0;
+            working=false;
+            mode=1;
             this.start_station_name=start_station_name;
             this.distance=0;
             this.direction=direction;
+            waiting_station_name=start_station_name;
         }
 
     }
@@ -174,7 +345,14 @@ public class Main {
         List<Station> getVertices() {
             return new ArrayList<>(vertices);
         }
-
+        Station getStation(char c){
+            for (Station st:vertices
+                 ) {
+                if (st.name == c)
+                    return st;
+            }
+            return null;
+        }
         boolean addVertex(Station vertex){
             return vertices.add(vertex);
         }
